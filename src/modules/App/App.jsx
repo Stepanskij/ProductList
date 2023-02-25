3ce0cdb450d127ada28ee695ae82da7acae4e7cd
Stepanskij <StepanskijH@yandex.ru>
2023-02-25 14:45:57 +0700

    fun

--- src/modules/App/App.jsx
+++ src/modules/App/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import block from "bem-cn";
 
 import ProductList from "features/ProductList";
 import Modal from "modules/Modal";
+// import UiKit from "modules/UiKit";
+const UiKit = React.lazy(() => import("modules/UiKit"));
 
 import "./App.scss";
 
@@ -17,6 +19,10 @@ class App extends React.Component {
     return (
       <div className={b()}>
         <ProductList />
+        {/* 
+        <Suspense fallback={<div>Загрузка...</div>}>
+          <UiKit />
+        </Suspense> */}
       </div>
     );
   }
